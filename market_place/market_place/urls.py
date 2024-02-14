be62c98646f7f6a95fcd6ea54a8025cd2dc80508
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2024-02-14 16:43:30 +0000

    Home page, Detail page for products

--- market_place/market_place/urls.py
+++ market_place/market_place/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static #Do not use in production
+from django.conf import settings #Do not use in production
 
 from core.views import index, contact
 
 urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
+    path("item/", include("item.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Do not use in production
